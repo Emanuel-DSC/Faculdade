5594bc6d899d8d6790d06956b62aba3c505d3734
Emanuel Costa <emanuelldsc@gmail.com>
2025-03-25 03:05:20 +0000

    add densidade populacional e PIB per capita

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main() {
+
+    // carta 1 
     char estado1;
     char codigo1[4];
     char nomeCidade1[50];
@@ -31,6 +33,10 @@ int main() {
     printf("Digite o número de pontos turísticos: ");
     scanf("%d", &pontosTuristicos1);
 
+    float densidadePopulacional1 = populacao1 / area1;
+    float pibPerCapita1 = pib1 * 1000000000 / populacao1;
+
+    // carta 2
     char estado2;
     char codigo2[4];
     char nomeCidade2[50];
@@ -61,6 +67,11 @@ int main() {
     printf("Digite o número de pontos turísticos: ");
     scanf("%d", &pontosTuristicos2);
 
+    float densidadePopulacional2 = populacao2 / area2;
+    float pibPerCapita2 = pib2 * 1000000000 / populacao2;
+
+    //Resultado das cartas
+
     printf("\nCarta 1:\n");
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
@@ -69,6 +80,8 @@ int main() {
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional1);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita1);
 
     printf("\nCarta 2:\n");
     printf("Estado: %c\n", estado2);
@@ -78,6 +91,8 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional2);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita2);
 
     return 0;
 }
